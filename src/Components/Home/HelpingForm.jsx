f268667784c30e58a41978ef3414bd4618c9d71c
het patel <hetpatel140505@gmail.com>
2025-08-17 22:15:32 +0530

    Fixed footer layout issue

--- src/Components/Home/HelpingForm.jsx
+++ src/Components/Home/HelpingForm.jsx
@@ -3,27 +3,32 @@ import Input from "../Input/Input";
 
 function HelpingForm() {
 
-return (
-  <>
+  return (
+    <>
       <div className="relative h-screen w-full">
-  <h1 className="font-playfair font-bold text-5xl sm:text-4xl md:text-6xl lg:text-9xl
-   ml-6 sm:ml-20 lg:ml-80 lg:mt-7 mt-3  absolute whitespace-nowrap">
-  Having Trouble
-</h1>
+        <h1 className="font-playfair font-bold text-5xl sm:text-4xl md:text-6xl lg:text-9xl
+                       ml-6 sm:ml-20 lg:ml-80 lg:mt-7 mt-3  absolute whitespace-nowrap">
+          Having Trouble
+        </h1>
 
-  <h1 className="font-playfair text-4xl sm:text-3xl md:text-7xl lg:text-8xl
-   ml-6 sm:ml-20 lg:ml-80 lg:mt-50 mt-19  absolute whitespace-nowrap">
-    What To Buy ?
-  </h1>
-  <div className="bg-[var(--color-aboutbg)] ml-auto mt-70 lg:mt-100 h-35 lg:h-80 w-65 lg:w-290">
-  </div>
-    <h1 className="justify-center text-center mt-3 text-3xl font-inter">
-      Feel free to contact us 
-    </h1>
-</div>
+        <h1 className="font-playfair text-4xl sm:text-3xl md:text-7xl lg:text-8xl
+                       ml-6 sm:ml-20 lg:ml-80 lg:mt-50 mt-19  absolute whitespace-nowrap">
+          What To Buy ?
+        </h1>
+        <div className="bg-[var(--color-aboutbg)] ml-auto mt-70 lg:mt-100 h-35 lg:h-80 w-65 lg:w-290">
+        </div>
+        <h1 className="font-montserrat font-bold text-center mt-6 text-[10px] sm:text-3xl lg:text-2xl
+                       text-gray-700 tracking-wide leading-relaxed max-w-4xl mx-auto px-4">
+          Drop your number and our experts will reach out to you soon !
+        </h1>
+
+          <Input 
+           
+          />
+      </div>
 
     </>
-)
+  )
 }
 
 export default HelpingForm;                 
--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,28 +1,29 @@
-import React from 'react';       
+import React from 'react';
 
-function Input({
-    type, 
-    label, 
-    value , 
-    onChange,
-    className
-}, ...props){
-
-    return(
-        <>
-        <div className='mb-4'>
-            <input
-                type = {type}
-                label = {label}
-                value={value}
-                onChange={onChange}
-                className={`border p-2 rounded w-full ${className}`}
-                {...props}
-            >
-                
-            </input>
-        </div>
-        </>
-    )
+function Input({ 
+  type = "text", 
+  label, 
+  value, 
+  onChange, 
+  className = "", 
+  ...props 
+}) {
+  return (
+    <div className="mb-4">
+      {label && (
+        <label className="block mb-2 font-medium text-gray-700">
+          {label}
+        </label>
+      )}
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        className={`border p-2 rounded w-full ${className}`}
+        {...props}
+      />
+    </div>
+  );
 }
+
 export default Input;
