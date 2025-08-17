f2ec7e7c972524c8cd8b1ed40c8ea97f6102b713
het patel <hetpatel140505@gmail.com>
2025-08-17 22:01:26 +0530

    Fixed footer layout issue

--- src/Components/Home/HelpingForm.jsx
+++ src/Components/Home/HelpingForm.jsx
@@ -3,14 +3,27 @@ import Input from "../Input/Input";
 
 function HelpingForm() {
 
-    return (
+return (
   <>
-        <div className="bg-[var(--color-aboutbg)] mt-42 h-25 w-210">
-           <h1>hello</h1>
-        </div>
+      <div className="relative h-screen w-full">
+  <h1 className="font-playfair font-bold text-5xl sm:text-4xl md:text-6xl lg:text-9xl
+   ml-6 sm:ml-20 lg:ml-80 lg:mt-7 mt-3  absolute whitespace-nowrap">
+  Having Trouble
+</h1>
 
-  </>
+  <h1 className="font-playfair text-4xl sm:text-3xl md:text-7xl lg:text-8xl
+   ml-6 sm:ml-20 lg:ml-80 lg:mt-50 mt-19  absolute whitespace-nowrap">
+    What To Buy ?
+  </h1>
+  <div className="bg-[var(--color-aboutbg)] ml-auto mt-70 lg:mt-100 h-35 lg:h-80 w-65 lg:w-290">
+  </div>
+    <h1 className="justify-center text-center mt-3 text-3xl font-inter">
+      Feel free to contact us 
+    </h1>
+</div>
+
+    </>
 )
 }
 
-export default HelpingForm;
+export default HelpingForm;                 
