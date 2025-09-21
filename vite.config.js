e5dfce3ec1d8cb0538737c1a09a2d56bb04761a3
HetSoni_Nakshatraloka_UI <hettsoni910@gmail.com>
2025-09-21 16:15:21 +0530

    fix: correct vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
   server: {
-    proxy:{
-      'api' : 'http://localhost:5173'
+    proxy: {
+      '/api': 'http://localhost:5173',  // fix: add / before api
     },
     hmr: {
       overlay: false,
     },
+    allowedHosts: ['nakshatraloka.com'], // 👈 allow your domain
+    host: '0.0.0.0', // 👈 optional: listen on all addresses
   },
   plugins: [
     tailwindcss(),
